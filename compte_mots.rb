de429f5571b22abd687bec785de707fd16d9cbaf
D.Coloma <dnl.coloma@gmail.coma>
2018-01-15 22:53:03 +0100

    Exo 3 done

--- compte_mots.rb
+++ compte_mots.rb
@@ -1,16 +1,15 @@
 def jean_michel_data(corpus,dictionnaire)
-    corpus_array=[]
+    #corpus_array=[]
     corpus.downcase!
-=begin
+
     corpus_array = corpus.split(/[,?;.:!' ]/)
     corpus_array.delete("")
-    corpus_array.each do |word|
-        puts "#{word} => #{dictionnaire.count(word)}"
-    end
-=end
-    #puts dictionnaire.count(corpus)
+    corpus = corpus_array.join(" ")
     dictionnaire.each do |word|
-        puts "#{word} : #{corpus.scan(word).count}" if corpus.scan(word).count > 0
+        compte = corpus.scan(word).count
+        if compte > 0
+            puts "#{word} : #{compte}"
+        end
     end
 end
 
@@ -20,3 +19,5 @@ end
 dictionary = ["below", "down", "go", "going", "horn", "how", "howdy", "it", "i", "low", "own", "part", "partner", "sit"]
 #jean_michel_data("below", dictionary)
 jean_michel_data("Howdy partner, sit down! How's it going?", dictionary)
+
+#{"down"=>1, "how"=>2, "howdy"=>1,"go"=>1, "going"=>1, "it"=>2, "i"=> 3, "own"=>1,"part"=>1,"partner"=>1,"sit"=>1}
